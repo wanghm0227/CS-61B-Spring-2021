e49d82b9c0a640698794a57959e4a6ccdee9758f
Haimei <1069022119@qq.com>
2021-02-01 15:40:41 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,9 +2,22 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
+    /** Returns the next number. */
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return n * 3 + 1;
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
+        while (n != 1) {
+            System.out.print(n + " ");
+            n = nextNumber(n);
+        }
+        System.out.print(n);
 
     }
 }
