4baae3879bc365940481bc492b1f91eb50fbcf2d
Haimei <1069022119@qq.com>
2021-03-04 09:19:59 +0800

    finished proj1ec

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,58 @@
+package tester;
+
+import static org.junit.Assert.*;
+
+import afu.org.checkerframework.checker.signature.qual.SourceName;
+import edu.princeton.cs.introcs.StdRandom;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
+    @Test
+    public void randomizedRemoveFirstTest() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<>();
+
+        for (int i = 0; i < 10; i += 1) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+
+            if (numberBetweenZeroAndOne < 0.5) {
+                sad.addLast(i);
+                ad.addLast(i);
+            } else {
+                sad.addFirst(i);
+                ad.addFirst(i);
+            }
+        }
+
+        for (int j = 0; j < 10; j += 1) {
+            Integer expected = ad.removeFirst();
+            Integer actual = sad.removeFirst();
+            assertEquals("Expected: " + expected + " but got: " + actual, expected, actual);
+        }
+    }
+
+    @Test
+    public void randomizedRemoveLastTest() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<>();
+
+        for (int i = 0; i < 10; i += 1) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+
+            if (numberBetweenZeroAndOne < 0.5) {
+                sad.addLast(i);
+                ad.addLast(i);
+            } else {
+                sad.addFirst(i);
+                ad.addFirst(i);
+            }
+        }
+
+        for (int j = 0; j < 10; j += 1) {
+            Integer expected = ad.removeLast();
+            Integer actual = sad.removeLast();
+            assertEquals("Expected: " + expected + " but got: " + actual, expected, actual);
+            }
+        }
+}
