4f5cf79887edeeeeb27c69fb3010e05090b4ee63
Haimei <1069022119@qq.com>
2021-04-09 18:00:17 +0800

    fixed Collatz file in lab01

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,26 +2,24 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-
-    /** Buggy implementation of nextNumber! */
+    /** Returns the next number. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return n * 3 + 1;
         }
     }
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
+
         while (n != 1) {
-            n = nextNumber(n);
             System.out.print(n + " ");
+            n = nextNumber(n);
         }
-        System.out.println();
+        System.out.print(n);
+
     }
 }
 
